0ff658873c3925845e03af77bbd4d715157a97c0
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-04-30 21:15:58 +0530

    update

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleImageUrl(req,res)})
 
-app.listen(process.evnv.PORT||3000,()=>{
-	console.log(`This app is working nicely${PORT}`)
+app.listen(process.env.PORT||3000,()=>{
+	console.log(`This app is working nicely on port ${PORT}`)
 })
