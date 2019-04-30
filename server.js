24ceb17eb97cad92f20cf0dbbe8892983e6a395e
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-04-30 20:46:34 +0530

    first commit

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/profile/:id',(req,res)=>{profile.handleProfile(req,res,db)})
 app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res)=>{image.handleImageUrl(req,res)})
 
-app.listen(3000,()=>{
-	console.log('This app is working nicely')
+app.listen(process.evnv.PORT||3000,()=>{
+	console.log(`This app is working nicely${PORT}`)
 })
