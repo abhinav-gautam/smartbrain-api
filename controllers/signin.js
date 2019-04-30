5c7691ca0306742759391de4997f96f81a125b58
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-04-30 22:45:16 +0530

    update

--- controllers/signin.js
+++ controllers/signin.js
@@ -7,7 +7,7 @@ const handleSignin=(req,res,db,bcrypt)=>{
 	.from('login')
 	.where('email',email)
 	.then(data=>{
-		if (data[0]===[]){
+		if (data[0].email===email){
 			return res.status(400).json('wrong credentials')
 		}
 		const isValid=bcrypt.compareSync(password, data[0].hash);
